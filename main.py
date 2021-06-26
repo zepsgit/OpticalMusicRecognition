ef5ef2cd21b8c336bdfa70a785705a0509b0d0ab
zepeng <zepengchen423@gmail.com>
2021-06-26 19:49:55 -0230

    integreate linear and nonlinear filter into one function

--- main.py
+++ main.py
@@ -2,24 +2,23 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-from spatial_filter import knl_generator, linear_filter, medf
+from spatial_filter import knl_generator, filter
 
 #test linear filter on 'salt-pepper noise' corrupted image
-''' 
-k=knl_generator('average', 3, 0)
-img=cv2.imread('02-3.png',0)
+
+k=knl_generator('laplacian', 0, 0)
+img=cv2.imread('D:\\lab2\\img2.png',0)
 cv2.imshow('02', img)
-img=linear_filter(k, img)
+img=filter(img,'linear',k)
 cv2.imshow('02-3f',img)
-cv2.imwrite('02-3f_average_3.png',img)
+#cv2.imwrite('02-3f_average_3.png',img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 '''
 #test median filter on 'salt-pepper noise'
 img=cv2.imread('testcases\\02.png',0)
-
 img=medf(3,img)
 cv2.imshow('02-f', img)
 cv2.imwrite('testcases\02-f_median_3.png',img)
 cv2.waitKey(0)
-cv2.destroyAllWindows()
+cv2.destroyAllWindows()'''
--- spatial_filter.py
+++ spatial_filter.py
@@ -12,39 +12,55 @@ def gkern(l, sig):
 
 #kernel generator
 '''
-type='average'|'gaussian'|'sobel_h'|'sobel_v'|'laplacian'
+type='average'|'gaussian'|'sobel_h'|'sobel_v'|'laplacian'|'median'
 size=odd number like 3,5,7...
 sigma >=0
 '''
 def knl_generator(type, size, sigma):
     if type=='average':
-        return np.ones([size,size],dtype=int)/size*size
+        return np.ones([size,size],dtype=int)/(size*size)
     if type=='gaussian':
         return gkern(size, sigma)
-    if type=='sobel_h':
-        return np.array([[-1,0,1],[-2,0,2],[-1,0,1]]) 
     if type=='sobel_v':
+        return np.array([[-1,0,1],[-2,0,2],[-1,0,1]]) 
+    if type=='sobel_h':
         return np.array([[-1,-2,-1],[0,0,0],[1,2,1]])
     if type=='laplacian':
         return np.array([[0.4038,    0.8021,    0.4038],
                         [0.8021,   -4.8233,    0.8021],
                         [0.4038,    0.8021,    0.4038]])
+    if type=='median':
+        return np.zeros([size,size])
 
-def linear_filter(kx, img):
+'''
+function: spatial filter including linear and non-linear filter
+input:img--np.array|type--'linear' or 'nonlinear'|median_size--the size of median filter
+output:filtered image--np.array
+'''
+def filter(img, type, kx):
     m,n=img.shape
-    kernel_n=kx.shape[0]
-    kn=int((kernel_n-1)/2)
-    img_=np.zeros([m,n])
-    img_new=np.zeros([m+kernel_n,n+kernel_n])
-    for s in range(kn,m+kn):
-        for t in range(kn,n+kn):
-            img_new[s,t]=img[s-kn,t-kn]
-            sum_p=0
-            for p in range(0,kernel_n):
-               for q in range(0,kernel_n):
-                    sum_p=sum_p+img_new[s-kn+p,t-kn+q]*kx[p,q]
-                    img_[s-kn,t-kn]=sum_p
-    return img_
+    size=kx.shape[0]
+    pad_len=int((size-1)/2)
+    new_img=np.zeros([m,n],dtype=np.uint8)
+    padded_img=pad_img(img, size)
+    for i in range(pad_len,m+pad_len):
+        for j in range(pad_len,n+pad_len):
+            sum_prod=0
+            for s in range(0,size):
+               for t in range(0,size):
+                   p=i-pad_len+s
+                   q=j-pad_len+t
+                   if type=='linear':
+                       sum_prod=sum_prod+padded_img[p,q]*kx[s,t]
+                   if type=='nonlinear':
+                       kx[s,t]=padded_img[p, q]
+            if type=='linear':
+                new_img[i-pad_len,j-pad_len]=sum_prod
+            if type=='nonlinear':
+                sorted_kx=np.sort(kx,axis=None,kind='quicksort')
+                med=sorted_kx[int((size*size-1)/2)]
+                new_img[i-pad_len, j-pad_len]=med
+    return new_img
 
 #pad image border with 0
 #size denote kernel size
@@ -56,25 +72,3 @@ def pad_img(img,size):
         for j in range(pad_len, pad_len+n):
             padded_img[i,j]=img[i-pad_len,j-pad_len]
     return padded_img
-
-#non-linear filter:median filter
-def medf(size,img):
-    m,n=img.shape
-    kernel=np.zeros([size,size])
-    pad_len=int((size-1)/2)
-    padded_img=pad_img(img, size)
-    new_img=np.zeros([m,n],dtype=np.uint8)
-    for i in range(pad_len,pad_len+m):
-        for j in range(pad_len,pad_len+n):
-            for s in range(0,size):
-                for t in range(0,size):
-                    p=i-pad_len+s
-                    q=j-pad_len+t
-                    kernel[s,t]=padded_img[p,q]
-                    #print('s',s,'t',t, 'i',p,'j',q)
-            sorted_knl=np.sort(kernel, axis=None, kind='quicksort')
-            #print(sorted_knl)
-            med=sorted_knl[int((size*size-1)/2)]
-            new_img[i-pad_len,j-pad_len]=med
-    return new_img
-    
